e59c0c734f85a7b1cd3753b6053196af1964ca04
F9539235 Vinicius Becker De Souza <vinicius.souza.235@bb.com.br>
2025-02-03 12:21:01 -0300

    update

--- backend-remoto/main.tf
+++ backend-remoto/main.tf
@@ -1,27 +1,27 @@
 terraform {
   backend "s3" {
     bucket = "descomplicando-terraform-vinicius-becker"
-    key    = "aula_backend"
+    key    = "aula_backend2"
     region = "us-west-2"
   }
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t3.micro"
 
   tags = {
     Name = "HelloWorld"
   }
-  
+
 }
