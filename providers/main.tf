cb2acd4c3da27481ada81244c07a8c6b04816ddc
F9539235 Vinicius Becker De Souza <vinicius.souza.235@bb.com.br>
2025-02-03 12:44:44 -0300

    update

--- providers/main.tf
+++ providers/main.tf
@@ -12,16 +12,19 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "us-west-2"
-}
-
 resource "aws_instance" "web" {
   ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t3.micro"
-
+  provider = aws.east
   tags = {
-    Name = "HelloWorld"
+    Name = "East"
   }
-
+}
+resource "aws_instance" "web" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t3.micro"
+  provider = aws.sp
+  tags = {
+    Name = "SP"
+  }
 }
