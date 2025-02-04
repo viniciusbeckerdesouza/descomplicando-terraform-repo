4fb57ecb554dbe6f55ad4fbba0b96db58da35ccc
F9539235 Vinicius Becker De Souza <vinicius.souza.235@bb.com.br>
2025-02-04 09:48:13 -0300

    update

--- variables/variables.tf
+++ variables/variables.tf
@@ -2,4 +2,8 @@ variable "image_id" {
   type = string
   #default = "ami-08d70e59c07c61a3a"
   description = "O ID da AMI usada"
+    validation {
+    condition     = length(var.image_id) > 4 && substr(var.image_id, 0, 4) == "ami-"
+    error_message = "O image_id deve ser um identificador de AMI válido, começando com \"ami-\"."
+  }
 }
